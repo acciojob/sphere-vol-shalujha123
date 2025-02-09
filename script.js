f804a89490c3b3d07533c9f9d4a31151d3658fee
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-02-09 21:21:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function volume_sphere() {
+let radius = document.getElementById("radius")
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault()
+	let r = parseFloat(radius.value)
+	let v = (4/3)*Math.PI*Math.pow(r,3)
+	let volume = document.getElementById("volume")
+	volume.value = v.toFixed(4)
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
